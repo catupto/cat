ce5c6f95278392541986884ae7d695d48f82f0e8
catupto <yhxxm01@gmail.com>
2024-08-10 22:53:52 +0800

    Update 非凡资源.js

--- js/非凡资源.js
+++ js/非凡资源.js
@@ -6,7 +6,7 @@ var rule = {
     host: 'http://cj.ffzyapi.com',
     // homeTid: '13',
     homeTid: '',
-    cate_exclude: '电影片|连续剧|综艺片|动漫片|电影解说|体育|演员|新闻资讯',
+    cate_exclude: '电影片|伦理片|连续剧|综艺片|动漫片|电影解说|体育|演员|新闻资讯',
     // parse_url: 'json:http://jx.84jia.com/m3u8ts.php?url=',
     parse_url: '',
 }
