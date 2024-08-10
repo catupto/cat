6aa493d65b9b5d42072076684583f8100a8c8dcf
catupto <yhxxm01@gmail.com>
2024-08-10 22:49:31 +0800

    Update ok资源.js

--- js/ok资源.js
+++ js/ok资源.js
@@ -7,6 +7,6 @@ var rule = {
     host: 'https://api.okzy.org',
     // homeTid: '13',
     homeTid: '',
-    cate_exclude: '电影|电视剧|综艺|动漫|体育赛事|斯诺克',
+    cate_exclude: '电影|港台三级|日本伦理|韩国伦理|西方伦理|两性课堂|写真热舞|女频恋爱|电视剧|综艺|动漫|体育赛事|斯诺克',
     parse_url: '',
 }
