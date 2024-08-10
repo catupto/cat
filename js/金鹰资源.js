fbd753a86bf9ab948708910fce7df19b803ce1d9
catupto <yhxxm01@gmail.com>
2024-08-11 06:47:27 +0800

    Update 金鹰资源.js

--- js/金鹰资源.js
+++ js/金鹰资源.js
@@ -6,6 +6,6 @@ var rule = {
     host: 'https://jyzyapi.com',
     // homeTid: '13',
     homeTid: '',
-    cate_exclude: '电影片|伦理片|连续剧|综艺片|动漫片|电影解说|体育|演员|新闻资讯',
+    cate_exclude: '电影|伦理片|电视剧|综艺片|动漫片|电影解说|体育|演员|新闻资讯',
     parse_url: '内地剧|综艺',
 }
