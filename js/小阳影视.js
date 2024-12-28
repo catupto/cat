8f0342e902f3a6f88b1dfd5cfa135f983eae9277
catupto <yhxxm01@gmail.com>
2024-12-28 09:50:32 +0800

    Add files via upload

---
+++ js/小阳影视.js
@@ -0,0 +1,32 @@
+var rule = {
+    title: '小阳影视',
+    模板: 'mxpro',
+    host: 'https://xn--yety66k.eu.org',
+    url:'/index.php/vod/show/id/fyclass/page/fypage.html'
+    filterable: 0,
+    lazy: `js:
+		var html = JSON.parse(request(input).match(/r player_.*?=(.*?)</)[1]);
+		var url = html.url;
+		if (html.encrypt == '1') {
+			url = unescape(url)
+		} else if (html.encrypt == '2') {
+			url = unescape(base64Decode(url))
+		}
+		if (/\\.m3u8|\\.mp4/.test(url)) {
+			input = {
+				jx: 0,
+				url: url,
+				parse: 0
+			}
+		} else if (/\\/share/.test(url)) {
+			url = getHome(url) + request(url).match(/main.*?"(.*?)"/)[1];
+			input = {
+				jx: 0,
+				url: url,
+				parse: 0
+			}
+		} else {
+			input
+		}
+	`
+}
